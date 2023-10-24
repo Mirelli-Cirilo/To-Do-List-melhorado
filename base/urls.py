fc449ba19585512f66fe5a54cbbe632c4a7819ed
Maria Mirelli Cirilo <mirellicirilo44@gmail.com>
2023-10-23 23:40:36 -0300

    'prod'

--- base/urls.py
+++ base/urls.py
@@ -2,17 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('login/', views.loginPage, name="login"),
-    path('logout/', views.logoutPage, name="logout"),
-    path('registro/', views.registro, name="registrar"),
-    
-
-
-    path('', views.listaTarefa, name='tarefa'),
-
-    path('detalhes/<str:pk>/', views.detalhesTarefa, name='detalhes'),
-    path('adicionar/', views.adicionarTarefa, name='adicionar-tarefa'),
-    path('atualizar/<str:pk>/', views.atualizarTarefa, name='atualizar-tarefa'),
-    path('apagar/<str:pk>/', views.apagarTarefa, name='apagar-tarefa')
+    path('', views.loginPage, name='login'),
 
 ]    
